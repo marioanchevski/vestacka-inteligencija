a5c6298d33f86d0d2b206359e67d318971dd0945
anchevski_m <marioancevski25@gmail.com>
2020-03-18 16:03:32 +0100

    Deleted unnecessary code

--- course/laboratoriski-vezbi/prva/3.Pacman.py
+++ course/laboratoriski-vezbi/prva/3.Pacman.py
@@ -17,118 +17,84 @@ class Game:
 
 class Pacman:
     def __init__(self,map):
-        self.mapa=map[0]
         self.player = Player([0,0])
         self.game = Game(map[0],map[1],map[2])
 
     def food_left(self):
-
         for i in range(self.game.size_x):
             for j in range(self.game.size_y):
-                if self.mapa[i][j]=='.':
+                if self.game.map[i][j]=='.':
                     return True
         return False
 
-    def printi(self):
-        str1 = ""
-        for i in range(3):
-            for j in range(3):
-                str1 += self.mapa[i][j]
-            str1 += "\n"
-        print(str1)
-
-        for i in range(3):
-            for j in range(3):
-                str1+=self.mapa[i][j]
 
     def play_game(self):
-        next_move=[]
 
         cur_x=self.player.x
         cur_y=self.player.y
-        position=[]
-        hesoyam=0
+        map_has_dots=False
+
         while(self.food_left()):
-            hesoyam+=1
-            #dole
-            next_move=[]
+            map_has_dots=True
+            next_move = []  # list of next possible move ['value',[x,y]]
+            #down
             if cur_x+1<self.game.size_x:
-                next_move.append([self.mapa[cur_x+1][cur_y],[cur_x+1,cur_y]])
-            #nagore
+                next_move.append([self.game.map[cur_x+1][cur_y],[cur_x+1,cur_y]])
+            #up
             if cur_x-1>=0:
-                next_move.append([self.mapa[cur_x-1][cur_y],[cur_x-1,cur_y]])
-            #desno
+                next_move.append([self.game.map[cur_x-1][cur_y],[cur_x-1,cur_y]])
+            #right
             if cur_y+1<self.game.size_y:
-                next_move.append([self.mapa[cur_x][cur_y+1],[cur_x,cur_y+1]])
-            #levo
+                next_move.append([self.game.map[cur_x][cur_y+1],[cur_x,cur_y+1]])
+            #left
             if cur_y-1>=0:
-                next_move.append([self.mapa[cur_x][cur_y-1],[cur_x,cur_y-1]])
+                next_move.append([self.game.map[cur_x][cur_y-1],[cur_x,cur_y-1]])
 
-
+            #broj na posakuvani cekori
             num_dot=0
-
-            #print(str(next_move)+"nextt")
             for i in range(len(next_move)):
                 if next_move[i][0]==".":
                     num_dot += 1
 
             if num_dot==1:
-
                 for i in range(len(next_move)):
                     if next_move[i][0] == ".":
-                        #self.printi()
-                        #print(str(next_move) + " mozni potezi1")
                         cur_x=next_move[i][1][0]
                         cur_y=next_move[i][1][1]
-                        position=[cur_x,cur_y]
-                        self.player.move(position)
+                        self.player.move([cur_x,cur_y])
                         self.game.map[cur_x][cur_y]="#"
-                        next_move=[]
                         break
             elif num_dot>1:
+                # p --> lista od site posakuvani potezi (kade vrednosta e '.')
                 p=[elem for elem in next_move if elem[0]=='.']
-                #print(str(p)+ " elems inp")
-                rand=random.randint(0,len(p)-1)
-                #self.printi()
-                #print(str(next_move) + " mozni potezi2")
-                cur_x = p[rand][1][0]
-                cur_y = p[rand][1][1]
-                position = [cur_x, cur_y]
-                self.player.move(position)
+                random_move=random.randint(0,len(p)-1)
+                cur_x = p[random_move][1][0]
+                cur_y = p[random_move][1][1]
+                self.player.move([cur_x, cur_y])
                 self.game.map[cur_x][cur_y] = "#"
-                next_move = []
-                p=[]
-
             else:
-               # self.printi()
-                #print(str(next_move) + " mozni potezi3")
-                rand=random.randint(0,len(next_move)-1)
-                cur_x = next_move[rand][1][0]
-                cur_y = next_move[rand][1][1]
-                position = [cur_x, cur_y]
-                self.player.move(position)
+                random_move=random.randint(0,len(next_move)-1)
+                cur_x = next_move[random_move][1][0]
+                cur_y = next_move[random_move][1][1]
+                self.player.move([cur_x, cur_y])
                 self.game.map[cur_x][cur_y] = "#"
 
-                next_move = []
-        if hesoyam==0:
+        if map_has_dots==False:
             print("Nothing to do here")
 
 
-
-
-
 def create_map():
-    n = int(input())
-    m = int(input())
+    visina = int(input())
+    sirina = int(input())
     row = []
     map = []
-    for i in range(n):
+    for i in range(visina):
         string = input()
-        for j in range(m):
+        for j in range(sirina):
             row.append(string[j])
         map.append(row)
         row = []
-    return [map,n,m]
+    return [map,visina,sirina]
 
 if __name__=="__main__":
     pacman =  Pacman(create_map())
