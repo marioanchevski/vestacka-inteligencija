d410046a5f5fc01184690e810616ce5f792d1b74
anchevski_m <marioancevski25@gmail.com>
2020-03-20 22:54:25 +0100

    Added better solution

--- course/laboratoriski-vezbi/prva/1.Gradebook.py
+++ course/laboratoriski-vezbi/prva/1.Gradebook.py
@@ -1,36 +1,32 @@
-def pecati():
-    cur_index = ""
+def pecati(students):
+
     flag = False
     pominati_indeksi = []
 
-    for j in range(len(list_students)):
-        cur_index = str(list_students[j]['indeks'])
-        for l in range(len(pominati_indeksi)):
-            if pominati_indeksi[l] == cur_index:
+    for i in range(len(students)):
+        cur_index = str(students[i]['indeks'])
+        for j in range(len(pominati_indeksi)):
+            if pominati_indeksi[j] == cur_index:
                 flag = True
                 break
         if flag == False:
-            print("Student: " + list_students[j]['ime'])
-            for x in range(len(list_students)):
-                if cur_index == list_students[x]['indeks']:
-                    print("\t" + list_students[x]['predmet'] + ": " + str(list_students[x]['ocenka']))
+            print("Student: " + students[i]['ime'])
+            for x in range(len(students)):
+                if cur_index == students[x]['indeks']:
+                    print("\t" + students[x]['predmet'] + ": " + str(students[x]['ocenka']))
             pominati_indeksi.append(cur_index)
             print()
         flag = False
 
-
-if __name__ == '__main__':
+def vnesi():
     studenti = {}
     list_students = []
     vnesi = input()
-    index = 0
     while vnesi != 'end':
         inf = vnesi.split(",")
         studenti['ime'] = inf[0] + " " + inf[1]
         studenti['indeks'] = inf[2]
-        index = str(inf[2])
         studenti['predmet'] = inf[3]
-
         bodovi = int(int(inf[4]) + int(inf[5]) + int(inf[6]))
         if bodovi >= 0 and bodovi <= 50:
             studenti['ocenka'] = 5
@@ -46,6 +42,9 @@ if __name__ == '__main__':
             studenti['ocenka'] = 10
         vnesi = input()
         list_students.append(dict(studenti))
+    return list_students
 
-    pecati()
+if __name__ == '__main__':
+    students=vnesi()
+    pecati(students)
 
