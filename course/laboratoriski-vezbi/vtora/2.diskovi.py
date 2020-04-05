573d2dc6858317d208046cd7930838e8c63e468b
anchevski_m <marioancevski25@gmail.com>
2020-04-05 18:32:24 +0200

    Not working

---
+++ course/laboratoriski-vezbi/vtora/2.diskovi.py
@@ -0,0 +1,94 @@
+from utils import Problem
+from uninformed_search import *
+
+def d1(position,lista):
+    if position+1<len(lista) and lista[position+1]=="#":
+        lista[position+1]=lista[position]
+        lista[position]="#"
+    return tuple(lista)
+def d2(position,lista):
+    if position+2<len(lista) and lista[position+2]=="#" and lista[position+1]!="#":
+        lista[position + 2] = lista[position]
+        lista[position] = "#"
+    return tuple(lista)
+
+def l1(position,lista):
+    if position-1>=0 and lista[position-1]=="#":
+        lista[position-1]=lista[position]
+        lista[position]="#"
+    return tuple(lista)
+
+def l2(position,lista):
+    if position+2>=0 and lista[position-2]=="#" and lista[position-1]!="#":
+        lista[position - 2] = lista[position]
+        lista[position] = "#"
+    return tuple(lista)
+
+
+class Diskovi(Problem):
+    def __init__(self,initial,goal):
+        super().__init__(initial,goal)
+
+    def successor(self, state):
+        successors = dict()
+        lista = list(state)
+
+        for i in range(len(lista)):
+            if lista[i]!="#":
+                st = "D1: Disk"+str(lista[i])
+                if i+1<len(lista) and lista[i+1]=="#":
+                    lista[i+1]=lista[i]
+                    lista[i]="#"
+                    successors[st]=tuple(lista)
+        for i in range(len(lista)):
+            if lista[i]!="#":
+                st = "D2: Disk"+str(lista[i])
+                if i+2<len(lista) and lista[i+2]=="#" and lista[i+1]!="#":
+                    lista[i+2]=lista[i]
+                    lista[i]="#"
+                    successors[st]=tuple(lista)
+        for i in range(len(lista)):
+            if lista[i]!="#":
+                st = "L1: Disk"+str(lista[i])
+                if i-1>=0 and lista[i-1]=="#":
+                    lista[i-1]=lista[i]
+                    lista[i]="#"
+                    successors[st]=tuple(lista)
+        for i in range(len(lista)):
+            if lista[i]!="#":
+                st = "L2: Disk"+str(lista[i])
+                if i-2>=0 and lista[i-2]=="#" and lista[i-1]!="#":
+                    lista[i-2]=lista[i]
+                    lista[i]="#"
+                    successors[st]=tuple(lista)
+        return successors
+
+    def actions(self, state):
+        return self.successor(state).keys()
+
+    def result(self, state, action):
+        return self.successor(state)[action]
+
+    def goal_test(self, state):
+        return state == self.goal
+
+
+if __name__ == '__main__':
+    n = int(input())
+    l = int(input())
+    lista=[]
+    for i in range(n):
+        lista.append(i+1)
+    for i in range(l-n):
+        lista.append("#")
+
+    cel = lista[:]
+    cel.reverse()
+
+    lista=tuple(lista)
+    cel=tuple(cel)
+
+
+    disk_problem = Diskovi(lista,cel)
+    answer = breadth_first_graph_search(disk_problem)
+    print(answer.solution())
